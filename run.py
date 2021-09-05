e3a28cb8afeaed25f26598458ee209a4f3656c91
NYYCS <nyystu@gmail.com>
2021-09-05 21:42:29 +0800

    fixed enviromental variables

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ import os
 
 from bot import Miffy
 
+
+if 'BOT_TOKEN' not in os.environ:
+  from dotenv import load_dotenv
+  load_dotenv()
+
 TOKEN = os.environ['BOT_TOKEN']
 
 miffy = Miffy(command_prefix='-', intents=discord.Intents.all())
