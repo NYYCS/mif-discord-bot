547211fcc1203954286955fb8c28431a15863a26
NYYCS <nyystu@gmail.com>
2021-09-12 18:38:54 +0800

    clean up

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ GUILD_ID = 879252094509518908
 
 class Miffy(commands.Bot):
 
+    def __init__(self, *args, **options):
+        super().__init__(*args, **options)
+        
     @property
     def guild(self):
         return self.get_guild(GUILD_ID)
