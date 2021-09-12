faf1f5671e7603ada04aec640868ce15b6600780
NYYCS <nyystu@gmail.com>
2021-09-12 18:39:31 +0800

    updated cogs list

--- run.py
+++ run.py
@@ -1,26 +1,28 @@
 import discord
+import discord.utils
 import os
 
 from bot import Miffy
 
 
 if 'BOT_TOKEN' not in os.environ:
-  from dotenv import load_dotenv
-  load_dotenv()
+  	from dotenv import load_dotenv
+  	load_dotenv()
 
 TOKEN = os.environ['BOT_TOKEN']
 
-miffy = Miffy(command_prefix='-', intents=discord.Intents.all())
+miffy = Miffy(command_prefix='-m', intents=discord.Intents.all())
 
 COGS = [
-  'cogs.roles',
-  'cogs.verification',
-  'cogs.confession',
-  'cogs.welcome'
+  	'cogs.roles',
+  	'cogs.verification',
+  	'cogs.confession',
+  	'cogs.welcome',
+  	'cogs.utility'
 ]
 
 for cog in COGS:
-  miffy.load_extension(cog)
+    miffy.load_extension(cog)
 
 
 miffy.run(TOKEN)
