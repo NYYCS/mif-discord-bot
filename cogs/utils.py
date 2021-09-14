aa747e1f7f2b3172501bcdf0c70855134a2fd3bf
NYYCS <nyystu@gmail.com>
2021-09-14 20:00:42 +0800

    added more valid roles

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,16 +1,34 @@
 from discord.ext import commands
+from discord.utils import get
 import discord
+
+ROLE_IDS = [
+    879253452432211968,
+    883958187114512435,
+    883958188074991647,
+    882817709136552018
+]
 
 class Utility(commands.Cog):
 
     def __init__(self, bot) -> None:
         self.bot = bot
+        self._roles = []
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self._roles = [
+            get(self.bot.guild.roles, id=role_id)
+            for role_id in ROLE_IDS
+        ]
 
     @commands.command(name='send')
-    @commands.is_owner()
-    async def send_command(self, ctx, channel: discord.channel.TextChannel, message: str):
-        embed = discord.Embed(description=message, color=0x00aaff)
-        await channel.send(embed=embed)
+    async def send_command(self, ctx, channel: discord.channel.TextChannel, message: discord.Message, title: str = ''):
+        print(self._roles)
+        if any((role in ctx.author.roles for role in self._roles)):
+            print(self._roles)
+            embed = discord.Embed(title=title, description=message.content, color=0x00aaff)
+            await channel.send(embed=embed)
 
 
 def setup(bot):
