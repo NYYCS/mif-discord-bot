87f3557156e3833bba3130527e64a97520be9f89
NYYCS <nyystu@gmail.com>
2021-09-18 16:01:49 +0800

    fixed bugs and added more functionality

--- cogs/rooms.py
+++ cogs/rooms.py
@@ -1,11 +1,13 @@
 from discord.ext import commands, tasks
 from discord.utils import get
+from async_timeout import timeout
 
 from collections import namedtuple
 from datetime import datetime, timedelta
+from typing import OrderedDict
 import asyncio
 
-from miffy.utils import Interval, TZINFO
+from miffy.utils import Interval, TZINFO, keypad_emoji
 
 RoomBucket = namedtuple('RoomBucket', 'name')
 
@@ -16,27 +18,35 @@ class Booking:
         self.members = members
         self.owner = members[0]
 
+    @property
+    def span(self):
+        fmt = '%I:%M%p'
+        start, end = self.interval.start.strftime(fmt), self.interval.end.strftime(fmt)
+        return f'{start}~{end}'
 
 class Room:
 
     def __init__(self, bot, **attrs):
         self.bot = bot
+        self.name = attrs.pop('name')
         self.channel = attrs.pop('channel')
         self.role = attrs.pop('role')
 
-        self.bookings = {}
+        self._bookings = OrderedDict()
         self.occupied = False
+
 
     @classmethod
     def from_bucket(cls, bot, bucket):
+        name = bucket.name
         channel = get(bot.guild.channels, name=bucket.name)
         role = get(bot.guild.roles, name=bucket.name)
 
-        return cls(bot, channel=channel, role=role)
+        return cls(bot, name=name, channel=channel, role=role)
 
     def create_booking(self, *, interval, members):
         key = interval.start.timestamp()
-        self.bookings[key] = booking = Booking(interval=interval, members=members)
+        self._bookings[key] = booking = Booking(interval=interval, members=members)
 
         self.bot.loop.create_task(self._worker(booking))
 
@@ -45,10 +55,10 @@ class Room:
     async def _worker(self, booking):
 
         interval = booking.interval
-        start, end = interval.start, interval.end
-        
+        key = interval.start.timestamp()
+
         for member in booking.members:
-            await self.bot.send(member, title='✅预约成功!', description=f'{self.channel.name} {start.time()} - {end.time()}')
+            await self.bot.send(member, title='✅预约成功!', description=f'{self.channel.name} `{booking.span}`')
 
         await interval.wait_until_start()
 
@@ -67,11 +77,11 @@ class Room:
         await asyncio.sleep(60)
         
         for member in booking.members:
-            if member.voice.channel == self.channel:
-                member.edit(voice_channel=None)
+            if member.voice is not None and member.voice.channel == self.channel:
+                await member.edit(voice_channel=None)
 
+        self._bookings.pop(key)
         self.occupied = False
-     
 
 class Rooms(commands.Cog):
 
@@ -85,17 +95,34 @@ class Rooms(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
-        self._rooms = {}
-        self._channel = None
-
+        self._rooms = OrderedDict()
+        self._channel = None  
+        self._lock = None
+    
         self.main_loop.start()
+        self.update_message_loop.start()
 
     def _fill_rooms(self):
-        fmt = '{0}\ufe0f\u20e3'
         for i, bucket in enumerate(self.ROOM_BUCKETS):
-            emoji = fmt.format(i)
+            emoji = keypad_emoji(i)
             self._rooms[emoji] = Room.from_bucket(self.bot, bucket)
-        
+
+    async def _update_message(self):
+        message = await self._channel.fetch_message(self.MESSAGE_ID)
+
+        embed = message.embeds[0].copy()
+        lines = ['**请点击您想要预约的房间所对应的表情:**']
+    
+        for emoji, room in self._rooms.items():
+            await message.add_reaction(emoji)
+            schedule = ' '.join(f'`{booking.span}`' for booking in room._bookings.values())
+            lines.append(f'{emoji} {room.name}: {schedule}')
+
+        description = '\n'.join(lines)
+        embed.description = description
+
+        await message.edit(embed=embed)
+
     async def get_interval(self, payload):
 
         def check(message):
@@ -106,7 +133,7 @@ class Rooms(commands.Cog):
 
         try:
             dt = datetime.strptime(message.content, '%I:%M%p')
-            start = datetime.now(TZINFO).replace(hour=dt.hour, minute=dt.hour)
+            start = datetime.now(TZINFO).replace(hour=dt.hour, minute=dt.minute)
         except ValueError:
             raise ValueError('给予的时间格式不正确！')
 
@@ -135,6 +162,19 @@ class Rooms(commands.Cog):
         ]
 
         return members
+    
+    async def _cleanup(self):
+        async for message in self._channel.history():
+            if message.id != self.MESSAGE_ID:
+                await message.delete()
+
+    @tasks.loop(minutes=5)
+    async def update_message_loop(self):
+        await self._update_message()
+
+    @update_message_loop.before_loop
+    async def before_update_message_loop(self):
+        await self.bot.wait_until_ready()
 
     @tasks.loop(hours=1)
     async def main_loop(self):
@@ -144,22 +184,40 @@ class Rooms(commands.Cog):
 
         while True:
             payload = await self.bot.wait_for('raw_reaction_add', check=check)
+
             room = self._rooms[payload.emoji.name]
-        
-            interval = await self.get_interval(payload)
+            member = self.bot.guild.get_member(payload.user_id)
 
-            if not interval.in_future():
-                raise ValueError('时间不能在过去！')
+            async with timeout(120) as tm, self.bot.lock_channel(self._channel, allowed_members=[member]):
+                interval = await self.get_interval(payload)
+                if not interval.in_future():
+                    raise ValueError('预约时间不可以在过去！')
 
-            members = await self.get_members(payload)
+                for booking in room._bookings.values():
+                    if interval.intersects(booking.interval):
+                        raise ValueError('预约的时间撞到了！')
+                    
+                members = await self.get_members(payload)
+
+            if tm.expired:
+                raise RuntimeError('等太久了，从新来过吧.')
 
             room.create_booking(interval=interval, members=members)
+
+            await self._cleanup()
+            await self._update_message()
 
     @main_loop.before_loop
     async def before_main_loop(self):
         await self.bot.wait_until_ready()
         self._channel = self.bot.get_channel(self.CHANNEL_ID)
         self._fill_rooms()
+
+    @main_loop.error
+    async def on_main_loop_error(self, exception):
+        await self.bot.send(self._channel, description=exception)
+        await self._cleanup()
+        self.main_loop.restart()
 
 def setup(bot):
     bot.add_cog(Rooms(bot))
