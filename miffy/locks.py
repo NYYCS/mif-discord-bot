8d0fd04ea477e53d816e95d5625a95001f19a1fe
NYYCS <nyystu@gmail.com>
2021-09-18 16:03:38 +0800

    added missing awaits

--- miffy/locks.py
+++ miffy/locks.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+from . import utils
+
 class ChannelLock:
 
     def __init__(self, bot, channel, *, allowed_members = None):
@@ -8,15 +10,15 @@ class ChannelLock:
         self.channel = channel
         self.overwrites = channel.overwrites
         self.allowed_members = allowed_members
-
+        self._locked = False
         self._role = None
 
     async def __aenter__(self):
         for target in self.overwrites.keys():
-            self.channel.set_permissions(target, send_message=False, add_reactions=False)
+            await self.channel.set_permissions(target, send_messages=False, add_reactions=False)
 
         if self.allowed_members:
-            self._role = await self.bot.guild.create_role(name=''.join(random.sample(string.ascii_letters, 6)))
+            self._role = await self.bot.guild.create_role(name=utils.random_hash(6))
             await self.channel.set_permissions(self._role, read_messages=True, send_messages=True, add_reactions=True)
 
             for member in self.allowed_members:
@@ -26,7 +28,7 @@ class ChannelLock:
 
     async def __aexit__(self, exc_type, exc, tb):
         for target, overwrite in self.overwrites.items():
-            self.channel.set_permissions(target, overwrite=overwrite)
+            await self.channel.set_permissions(target, overwrite=overwrite)
 
         if self._role:
             await self._role.delete()
