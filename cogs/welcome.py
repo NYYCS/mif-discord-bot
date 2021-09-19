03e7664a7e12d7dfb15f8e88efa57f71a78336cf
NYYCS <nyystu@gmail.com>
2021-09-20 01:48:06 +0800

    fixed embed not being sent properly

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -12,7 +12,7 @@ class Welcome(commands.Cog):
     async def on_member_join(self, member):
         channel = self.bot.get_channel(CHANNEL_ID)
         description = f'👋欢迎`{member}`来到【旦马公寓】!\n请记得检查你的私信进行注册！'
-        await channel.send(discord.Embed(description=description, color=0x00aaff))
+        await channel.send(embed=discord.Embed(description=description, color=0x00aaff))
 
 
 def setup(bot):
