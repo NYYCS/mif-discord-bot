6c86e7de6529853a8630ee3ede950c03d1d58b02
NYYCS <nyystu@gmail.com>
2021-09-21 13:40:24 +0800

    fixed embed issue

--- cogs/verification.py
+++ cogs/verification.py
@@ -13,15 +13,15 @@ class Verification(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        role = get(self.bot.guild.roles, name='访客')
-        await member.add_roles(role)
+        role = get(self.bot.guild.roles, id=883964905965887509)
 
         title='👋欢迎来到【旦马公寓】!'
         description='''请同学利用`-mregister 全名 名字`进行注册!
         例子：`-mregister 黄宇悦 20300246005`
         '''
-
+        
         await member.send(embed=discord.Embed(title=title, description=description, color=0x00aaff))
+        await member.add_roles(role)
     
     @commands.command(name='register')
     async def register_command(self, ctx, name: str, school_id: int):
@@ -29,7 +29,7 @@ class Verification(commands.Cog):
             channel = self.bot.get_channel(CHANNEL_ID)
             await channel.send(f'{ctx.author} - {name} {school_id}')
             description='✅注册成功！请稍等....'
-            await ctx.channel.send(discord.Embed(description=description, color=0x00aaff))
+            await ctx.channel.send(embed=discord.Embed(description=description, color=0x00aaff))
 
 def setup(bot):
     bot.add_cog(Verification(bot))
