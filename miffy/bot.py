a6836f8013e4d09810eaac3a806262cfdc767898
NYYCS <nyystu@gmail.com>
2021-09-26 21:12:44 +0800

    added extra functionality to send:

--- miffy/bot.py
+++ miffy/bot.py
@@ -1,10 +1,10 @@
-from discord import message
 from discord.ext import commands
 import discord
-
-from . import locks
+import asyncpg
 
 import os
+
+from . import locks
 
 
 GUILD_ID = 879252094509518908
@@ -18,14 +18,19 @@ class Miffy(commands.Bot):
         self.pool = None
 
     async def start(self, *args, **kwargs):
+        self.pool = await asyncpg.create_pool(POSTGRES_URI, ssl='require')
         return await super().start(*args, **kwargs)
     
     async def send(self, messageable, *, delete_after=None, **attrs):
+        content = attrs.pop('content', '')
         embed = discord.Embed(**attrs, color=0x00aaff)
-        await messageable.send(embed=embed, delete_after=delete_after)
+        await messageable.send(content, embed=embed, delete_after=delete_after)
 
     def lock_channel(self, channel, *, allowed_members = None):
         return locks.ChannelLock(self, channel, allowed_members=allowed_members)
+
+    async def on_command_error(self, ctx, exception):
+        await self.send(ctx, description=getattr(exception, 'original', exception))
         
     @property
     def guild(self):
