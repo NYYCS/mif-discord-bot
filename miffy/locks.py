cd15f3007dee24c00009a1f1e01f9a1975765423
NYYCS <nyystu@gmail.com>
2021-10-01 17:14:19 +0800

    made locked members be able to read messages

--- miffy/locks.py
+++ miffy/locks.py
@@ -15,11 +15,11 @@ class ChannelLock:
 
     async def __aenter__(self):
         for target in self.overwrites.keys():
-            await self.channel.set_permissions(target, send_messages=False, read_messages=False, add_reactions=False)
+            await self.channel.set_permissions(target, send_messages=False, add_reactions=False)
 
         if self.allowed_members:
             self._role = await self.bot.guild.create_role(name=utils.random_hash(6))
-            await self.channel.set_permissions(self._role, read_messages=True, send_messages=True, add_reactions=True)
+            await self.channel.set_permissions(self._role, send_messages=True, add_reactions=True)
 
             for member in self.allowed_members:
                 await member.add_roles(self._role)
